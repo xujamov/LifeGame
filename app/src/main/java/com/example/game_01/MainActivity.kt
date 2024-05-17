164f56c288388c6447f9b14c585b4c5d02d94343
Doniyor <doniyorprince95@gmail.com>
2024-05-17 13:40:07 +0500

    set house function

--- app/src/main/java/com/example/game_01/MainActivity.kt
+++ app/src/main/java/com/example/game_01/MainActivity.kt
@@ -9,7 +9,9 @@ import android.widget.ImageView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
+    private lateinit var imageHouse: ImageView
     private lateinit var button: Button
+    private lateinit var button2: Button
     private val imageList = arrayOf(
         R.drawable.transport_stat_modifier_1_full_size,
         R.drawable.transport_stat_modifier_2_full_size,
@@ -21,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         R.drawable.transport_stat_modifier_8_full_size,
         R.drawable.transport_stat_modifier_9_full_size
     )
+    private val imageHouses = arrayOf(
+        R.drawable.accommodation_stat_modifier_1_full_size,
+        R.drawable.accommodation_stat_modifier_2_full_size,
+        R.drawable.accommodation_stat_modifier_3_full_size,
+        R.drawable.accommodation_stat_modifier_4_full_size,
+        R.drawable.accommodation_stat_modifier_5_full_size,
+        R.drawable.accommodation_stat_modifier_6_full_size
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,22 +39,29 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize views
         imageView = findViewById(R.id.imageView2)
+        imageHouse = findViewById(R.id.imageHouse)
         button = findViewById(R.id.button)
+        button2 = findViewById(R.id.button2)
+
 
         // Set click listener for the button
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-// Change to a random image when the button is clicked
-                setRandomImage()
-            }
-        })
+        button.setOnClickListener {
+            // Set a random image
+            setRandomImage()
+        }
+        button2.setOnClickListener {
+            // Set a random image
+            setRandomImage2()
+        }
     }
 
     private fun setRandomImage() {
-        // Choose a random image from the list
         val randomImageResourceId = imageList.random()
-
-        // Set the chosen random image to the ImageView
         imageView.setImageResource(randomImageResourceId)
+    }
+
+    private fun setRandomImage2() {
+        val randomImageResourceId = imageHouses.random()
+        imageHouse.setImageResource(randomImageResourceId)
     }
 }
